f38443bd715ed24ef6cacc521ed4d9629d23c46f
dmunoza <diego.2015.movil@gmail.com>
2021-08-15 21:12:52 -0400

    auto-transform payloads to DTO instances

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-   whitelist: true, // It allows to return only the corresponding data, if another parameter is sent, it does not return it.
-   forbidNonWhitelisted: true, //allows to return message error to data no correspon 
+    whitelist: true, // It allows to return only the corresponding data, if another parameter is sent, it does not return it.
+    forbidNonWhitelisted: true, //allows to return message error to data no correspon 
+    transform: true, // enabling auto transform feature of validationPipe.
   }));
   await app.listen(3000);
 }
