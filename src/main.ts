4488db53acab78af720da68d3a75c0e817829dea
dmunoza <diego.2015.movil@gmail.com>
2021-08-15 21:03:02 -0400

    handling malicious request data.

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
    whitelist: true, // It allows to return only the corresponding data, if another parameter is sent, it does not return it.
-   forbidNonWhitelisted: true, //allows to return message error to data no corresponding. 
+   forbidNonWhitelisted: true, //allows to return message error to data no correspon 
   }));
   await app.listen(3000);
 }
