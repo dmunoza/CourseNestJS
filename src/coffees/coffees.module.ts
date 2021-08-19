f338facce245f006d6e7c68b5fac975ed5e38720
dmunoza <diego.2015.movil@gmail.com>
2021-08-19 18:30:12 -0400

    relations between entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { flatten, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeController } from 'src/coffe/coffe.controller';
 import { CoffeService } from 'src/coffe/coffe.service';
 import { Coffee } from 'src/coffe/entities/coffe.entity';
+import { Flavor } from 'src/coffe/entities/flavor.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Coffee])],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
     controllers: [CoffeController], 
     providers: [CoffeService]})
 export class CoffeesModule {}
